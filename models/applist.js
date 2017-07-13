dd783425d613acafe040ce845a6c6527f950f182
design510 <376205871@qq.com>
2017-07-13 22:00:19 +0800

    update PC test

--- models/applist.js
+++ models/applist.js
@@ -4,19 +4,19 @@ var db = require('./db'),
     base = db.base;
 
 var applistSchema = base.add({
-    app_title: String
-    //app_title: {type: String},
-    //app_downnum: {type: String},
-    //app_size: {type: String},
-    //app_version: {type: String},
-    //app_score_good: {type: Number},
-    //app_score_bad: {type: Number},
-    //app_commentnumber: {type: String},
-    //app_developer: {type: String},
-    //app_update: {type: String},
-    //app_icon: {type: String},
-    //app_download: {type: String},
-    //app_packagename: {type: String}
+    // app_title: String
+    app_title: {type: String},
+    app_downnum: {type: String},
+    app_size: {type: String},
+    app_version: {type: String},
+    app_score_good: {type: Number},
+    app_score_bad: {type: Number},
+    app_commentnumber: {type: String},
+    app_developer: {type: String},
+    app_update: {type: String},
+    app_icon: {type: String},
+    app_download: {type: String},
+    app_packagename: {type: String}
 });
 
 exports.ApplistModel = mongoose.model('appchinasofts', applistSchema, 'appchinasoft');
